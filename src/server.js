e022201b14f588b8b677f1c34bde00f77f91d55f
AnishChatti <177r1a05k1@gmail.com>
2020-09-16 21:05:04 +0530

    PATH ERROR FIXED-2

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.static(`${__dirname}../Sorting-visualizer`));
+app.use(express.static(`${__dirname}/../Sorting-visualizer`));
 
 app.listen(port, () => {
  console.log('Server is up on port ' + port)
